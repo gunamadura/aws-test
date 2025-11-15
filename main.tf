648f264a4611b0bdc914b6d13fd473f4acb0f11e
gunamadura <gunasekharmadura@gmail.com>
2025-11-16 01:38:21 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ resource "aws_s3_bucket" "bucket_dev" {
   }
 }
 
-resource "aws_instance" "guna_learning_instance" {
-  ami                     = "ami-02b8269d5e85954ef"
-  instance_type           = "t2.micro"
-  # host_resource_group_arn = "arn:aws:resource-groups:ap-south-1:270325283284:group/win-testhost"
-  # tenancy                 = "host"
-  tags = {
-    Name = "guna-learning-instance"
-  }
-}
+# resource "aws_instance" "guna_learning_instance" {
+#   ami                     = "ami-02b8269d5e85954ef"
+#   instance_type           = "t2.micro"
+#   # host_resource_group_arn = "arn:aws:resource-groups:ap-south-1:270325283284:group/win-testhost"
+#   # tenancy                 = "host"
+#   tags = {
+#     Name = "guna-learning-instance"
+#   }
+# }
